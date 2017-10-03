5bc5682acdcc589b62b1d30f8e5a225bd5eab933
Kartheek Chintalapati <carteak004@gmail.com>
2017-10-03 15:34:08 -0500

    initial commit

---
+++ EDirectory/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  EDirectory
+//
+//  Created by Kartheek chintalapati on 29/09/17.
+//  Copyright © 2017 Northern Illinois University. All rights reserved.
+//
+
+import UIKit
+
+class Employee: NSObject {
+    
+    
+}
